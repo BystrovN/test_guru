63b2271c0084f408130371c919e0534f7ad3e7a3
 Никита Быстров <hallnik96@gmail.com>
2024-01-30 12:13:24 +0300

    feat: lesson_3 0.3

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_01_29_194921) do
+ActiveRecord::Schema.define(version: 2024_01_29_192426) do
 
   create_table "answers", force: :cascade do |t|
     t.text "text", null: false
     t.integer "question_id", null: false
-    t.integer "user_id", null: false
     t.boolean "correct", default: false, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["question_id"], name: "index_answers_on_question_id"
-    t.index ["user_id"], name: "index_answers_on_user_id"
   end
 
   create_table "categories", force: :cascade do |t|
@@ -34,18 +32,18 @@ ActiveRecord::Schema.define(version: 2024_01_29_194921) do
     t.integer "test_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id", null: false
     t.index ["test_id"], name: "index_questions_on_test_id"
-    t.index ["user_id"], name: "index_questions_on_user_id"
   end
 
   create_table "tests", force: :cascade do |t|
     t.string "title", limit: 50, null: false
     t.integer "level", default: 1, null: false
     t.integer "category_id", null: false
+    t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["category_id"], name: "index_tests_on_category_id"
+    t.index ["user_id"], name: "index_tests_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -58,8 +56,7 @@ ActiveRecord::Schema.define(version: 2024_01_29_194921) do
   end
 
   add_foreign_key "answers", "questions"
-  add_foreign_key "answers", "users"
   add_foreign_key "questions", "tests"
-  add_foreign_key "questions", "users"
   add_foreign_key "tests", "categories"
+  add_foreign_key "tests", "users"
 end
