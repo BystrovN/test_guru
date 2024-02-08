6c6b9f48fd6ab54bf1abeae4b0b6415e4e55a3d2
 Никита Быстров <hallnik96@gmail.com>
2024-02-09 02:46:49 +0300

    feat: lesson_8 0.1

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,17 +1,16 @@
 class QuestionsController < ApplicationController
   before_action :find_test, only: [:index, :create, :new]
-  before_action :find_question, only: [:show, :destroy]
+  before_action :find_question, only: [:show, :destroy, :edit, :update]
+
+  before_action :find_test, only: [:index, :create, :new]
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
   def index
     @questions = @test.questions
-    render plain: @questions.map(&:body).join("\n")
   end
 
-  def show
-    render plain: @question.body
-  end
+  def show; end
 
   def new
     @question = @test.questions.build
@@ -20,7 +19,7 @@ class QuestionsController < ApplicationController
   def create
     @question = @test.questions.build(question_params)
     if @question.save
-      redirect_to question_path(@question)
+      redirect_to @question
     else
       render :new
     end
@@ -28,7 +27,17 @@ class QuestionsController < ApplicationController
 
   def destroy
     @question.destroy
-    render plain: 'Question deleted successfully'
+    redirect_to test_questions_path(@question.test)
+  end
+
+  def edit; end
+
+  def update
+    if @question.update(question_params)
+      redirect_to @question
+    else
+      render :edit
+    end
   end
 
   private
