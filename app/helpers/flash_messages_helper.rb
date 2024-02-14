fa7e3285265035b19df53bc788e7a40e11949ad5
 Никита Быстров <hallnik96@gmail.com>
2024-02-14 19:09:19 +0300

    feat: lesson_10 0.2

--- app/helpers/flash_messages_helper.rb
+++ app/helpers/flash_messages_helper.rb
@@ -1,9 +1,7 @@
 module FlashMessagesHelper
-  def display_flash_messages
-    if flash[:alert].present?
-      content_tag :p, flash[:alert], class: 'flash alert'
-    elsif flash[:notice].present?
-      content_tag :p, flash[:notice], class: 'flash notice'
-    end
+  def display_flash_messages(type)
+    return if flash[type].nil?
+
+    content_tag :p, flash[type], class: "flash #{type}"
   end
 end
