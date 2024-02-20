35c9795ef4b3fd42e7c29dfb7ab9447b53fea11c
 Никита Быстров <hallnik96@gmail.com>
2024-02-20 21:24:05 +0300

    feat: lesson_13 0.1

--- app/helpers/flash_messages_helper.rb
+++ app/helpers/flash_messages_helper.rb
@@ -2,6 +2,24 @@ module FlashMessagesHelper
   def display_flash_messages(type)
     return if flash[type].nil?
 
-    content_tag :p, flash[type], class: "flash #{type}"
+    bootstrap_class = bootstrap_class_for_flash_type(type)
+    content_tag :div, flash[type], class: bootstrap_class
+  end
+
+  private
+
+  def bootstrap_class_for_flash_type(type)
+    case type
+    when 'notice'
+      'alert alert-info'
+    when 'success'
+      'alert alert-success'
+    when 'error'
+      'alert alert-danger'
+    when 'alert'
+      'alert alert-warning'
+    else
+      "alert alert-#{type}"
+    end
   end
 end
