f880de058d006931792944c2ae213748d3c779c1
 Никита Быстров <hallnik96@gmail.com>
2024-03-01 03:29:15 +0300

    feat: lesson_17 0.14

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -118,12 +118,14 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   config.action_mailer.default_url_options = { host: 'test-guru-6xyp.onrender.com' }
-
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    adress: 'smtp.gmail.com',
-    port: 587,
-    user_name: ENV.fetch('SMTP_USERNAME', nil),
+    tls: true,
+    address: 'smtp.yandex.ru',
+    port: 465,
+    domain: 'yandex.ru',
+    user_name: ENV.fetch('SMTP_USER', nil),
     password: ENV.fetch('SMTP_PASSWORD', nil),
     authentication: 'plain',
     enable_starttls_auto: true
